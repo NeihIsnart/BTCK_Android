d258a97874d1763f8a19c969a85faccda7ea400d
NONAME <sihiena2nh@gmail.com>
2022-05-15 22:22:15 +0700

    BTCK

--- app/src/main/java/com/example/btck_android/HomeActivity.java
+++ app/src/main/java/com/example/btck_android/HomeActivity.java
@@ -2,13 +2,43 @@ package com.example.btck_android;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class HomeActivity extends AppCompatActivity {
+    private Button btnbatdau;
+    private Button btnthoat;
+    private Button btnBangXepHang;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
+        btnbatdau = (Button) findViewById(R.id.btn_batdau);
+        btnbatdau.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HomeActivity.this, MainActivity.class);
+                startActivity(intent);
+            }
+        });
+        btnBangXepHang = (Button) findViewById(R.id.btn_bangxephang);
+        btnBangXepHang.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(HomeActivity.this, Charts.class);
+                startActivity(intent);
+            }
+        });
+        btnthoat = (Button) findViewById(R.id.btn_thoat);
+        btnthoat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                finish();
+            }
+        });
     }
 }
